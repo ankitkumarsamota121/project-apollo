e77e69ce39bee5fec9b39bc61621d536442c0cfd
ankitkumarsamota121 <ankitkumarsamota121@gmail.com>
2020-11-18 16:44:00 +0530

    Styling

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import Root from './config/Root';
 import App from './views/App';
 
-// import './index.css';
+import 'styles/index.css';
 
 import * as serviceWorker from './serviceWorker';
 
